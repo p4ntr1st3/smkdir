3117247e7a8d4a069a5364ec0d10f3bf3299acde
p4ntr1st3 <106686986+p4ntr1st3@users.noreply.github.com>
2022-06-08 18:56:45 -0300

    Update smkdir.sh

--- smkdir.sh
+++ smkdir.sh
@@ -10,27 +10,29 @@ ciclo="yes"
 
 read -p "Bienvenido al gestor de carpetas y subcarpetas"
 read -p "Deseas crear una carpeta(1) o una carpeta con sub-carpetas(2)?" input_type
-        if (( $input_type == 1 )); then
-                read -p "Ingrese el nombre de la carpeta: " input_namecarpeta
-                mkdir -m 744 $input_namecarpeta
-                read -p "Carpeta $input_namecarpeta creada, adios!"
-        elif (( $input_type == 2 )); then
-                read -p "Ingrese el nombre de la carpeta: " input_namecarpeta
-                mkdir -m 744 $input_namecarpeta
-                cd $input_namecarpeta
-                read -p "Carpeta $input_namecarpeta creada!"
-                while [ $ciclo == "yes" ];
-                do                                                                                                                                                                                                                                                                                                           
-                        read -p "Desea crear una sub-carpeta? (1)PARA CREAR: " input_typesub                                                                                                                                                                                                                                 
-                        if (( $input_typesub == 1 )); then                                                                                                                                                                                                                                                                   
-                                read -p "Ingrese el nombre de la subcarpeta: " input_namesubcarpeta                                                                                                                                                                                                                          
-                                mkdir -m 744 $input_namesubcarpeta                                                                                                                                                                                                                                                           
-                                read -p "Carpeta $input_namesubcarpeta creada!"                                                                                                                                                                                                                                              
-                        else                                                                                                                                                                                                                                                                                                 
-                                read -p "El programa se cerrara"                                                                                                                                                                                                                                                             
-                                lista=no                                                                                                                                                                                                                                                                                     
-                        fi                                                                                                                                                                                                                                                                                                   
-                done                                                                                                                                                                                                                                                                                                         
-        else                                                                                                                                                                                                                                                                                                                 
-                read -p "Al no escoger una de las dos opciones el programa se cerrara"                                                                                                                                                                                                                                       
-        fi
+if (( $input_type == 1 )); then
+        read -p "Ingrese el nombre de la carpeta: " input_namecarpeta
+        mkdir -m 744 $input_namecarpeta
+         read -p "Carpeta $input_namecarpeta creada, adios!"
+elif (( $input_type == 2 )); then
+        read -p "Ingrese el nombre de la carpeta: " input_namecarpeta
+        mkdir -m 744 $input_namecarpeta
+        cd $input_namecarpeta
+        read -p "Carpeta $input_namecarpeta creada!"
+        while [ $ciclo == "yes" ];
+        do                                                                                                                                                                                                                                                                                                           
+                read -p "Desea crear una sub-carpeta? (1)PARA CREAR: " input_typesub                                                                                                                                                                                                                                 
+                if (( $input_typesub == 1 )); then                                                                                                                                                                                                                                                                   
+                        read -p "Ingrese el nombre de la subcarpeta: " input_namesubcarpeta                                                                                                                                                                                                                          
+                        mkdir -m 744 $input_namesubcarpeta                                                                                                                                                                                                                                                           
+                        read -p "Carpeta $input_namesubcarpeta creada!"                                                                                                                                                                                                                                              
+                else                                                                                                                                                                                                                                                                                                 
+                        read -p "El programa se cerrara"                                                                                                                                                                                                                                                             
+                        lista=no                                                                                                                                                                                                                                                                                     
+                fi                                                                                                                                                                                                                                                                                                   
+        done                                                                                                                                                                                                                                                                                                         
+else                                                                                                                                                                                                                                                                                                                 
+        read -p "Al no escoger una de las dos opciones el programa se cerrara"                                                                                                                                                                                                                                       
+fi
+
+#============FIN============
